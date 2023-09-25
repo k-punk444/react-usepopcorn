47cbaf055a4aafa655c54d77e8b4aa68b666bb7a
DevKai <irakoseimei4@gmail.com>
2023-09-25 19:25:40 +0900

    Prop Drilling

--- src/App.js
+++ src/App.js
@@ -51,19 +51,21 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+
   return (
     <>
-      <NavBar />
-      <Main />
+      <NavBar movies={movies} />
+      <Main movies={movies} />
     </>
   );
 
-  function NavBar() {
+  function NavBar({ movies }) {
     return (
       <nav className="nav-bar">
         <Logo />
         <Search />
-        <NumResults />
+        <NumResults movies={movies} />
       </nav>
     );
   }
@@ -79,32 +81,36 @@ export default function App() {
 
   function Search() {
     const [query, setQuery] = useState("");
-    <input
-      className="search"
-      type="text"
-      placeholder="Search movies..."
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-    />;
+    return (
+      <input
+        className="search"
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    );
   }
 
-  function NumResults() {
-    <p className="num-results">
-      Found <strong>X</strong> results
-    </p>;
+  function NumResults({ movies }) {
+    return (
+      <p className="num-results">
+        Found <strong>{movies.length}</strong> results
+      </p>
+    );
   }
 
-  function Main() {
+  function Main({ movies }) {
     return (
       <main className="main">
-        <ListBox />
+        <ListBox movies={movies} />
         <WatchedBox />
       </main>
     );
   }
 }
 
-function ListBox() {
+function ListBox({ movies }) {
   const [isOpen1, setIsOpen1] = useState(true);
   return (
     <div className="box">
@@ -114,14 +120,12 @@ function ListBox() {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList />}
+      {isOpen1 && <MovieList movies={movies} />}
     </div>
   );
 }
 
-function MovieList() {
-  const [movies, setMovies] = useState(tempMovieData);
-
+function MovieList({ movies }) {
   return (
     <ul className="list">
       {movies?.map((movie) => (
