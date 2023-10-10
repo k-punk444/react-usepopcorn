11dc68c5f82c6225c6f5d02273e64318e3156499
DevKai <irakoseimei4@gmail.com>
2023-10-10 21:16:41 +0900

    Improving Reusability with Props

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
   </React.StrictMode>
 );
